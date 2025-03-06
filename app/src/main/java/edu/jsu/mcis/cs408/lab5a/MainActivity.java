cc066bcdb9b4c014f0069d75d12572616089b3db
William Saint <wsaint@stu.jsu.edu>
2025-03-06 12:06:44 -0600

    First Commit.

---
+++ app/src/main/java/edu/jsu/mcis/cs408/lab5a/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.jsu.mcis.cs408.lab5a;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import edu.jsu.mcis.cs408.lab5a.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+}
